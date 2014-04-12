16e1ffbecf1c1ee7d74847545c677df6f02e52c9
smspezzano <smspezzano@gmail.com>
2014-04-12 12:26:32 -0700

    WIP routes_controller: trying to test for multiple routes

--- spec/controllers/routes_controller_spec.rb
+++ spec/controllers/routes_controller_spec.rb
@@ -4,7 +4,6 @@ describe RoutesController do
 
 	describe "GET new" do
 
-
 		before(:each) do
 			@user = mock_model("User")
 			allow(User).to receive(:new).and_return(@user)
@@ -17,6 +16,15 @@ describe RoutesController do
 			get :new
 			expect(assigns(:route)).to eq(route)
 		end
+	end
+
+	describe "create new" do
+
+		before(:each) do
+			@user = mock_model("User")
+			allow(User).to receive(:new).and_return(@user)
+			allow(User).to receive(:find_by_remember_token).and_return(@user)
+		end
 
 		it 'should create a new route with the right user id' do
   		route = stub_model Route, 
@@ -26,6 +34,22 @@ describe RoutesController do
   			user_id: @user.id
 
   		route.user_id.should eql(@user.id)
+		end
+
+		it "should be able to create multiple routes" do
+			route1 = stub_model Route,
+  			startLocation: '123 Test St, Test, CA', 
+  			endLocation: '321 Foo, Bar, FO', 
+  			roadGradeStrategy: 'AVOID_ALL_HILLS', 
+  			user_id: @user.id
+  		
+  		route2 = stub_model Route,
+				startLocation: '123 Test St, Test, CA', 
+  			endLocation: '321 Foo, Bar, FO', 
+  			roadGradeStrategy: 'FAVOR_ALL_HILLS', 
+  			user_id: @user.id
+			binding.pry
+			expect(@user.routes.count).to be 2
 		end
 
 	end
